27932a4f78cae8bcd4640410600e499aaa276e3b
Gruselhaus <nico@gruselhaus.com>
2021-11-17 00:44:56 +0100

    Adding the kubernetes health checks

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const express = require("express");
 
 const app = express();
 
+const health = require("@cloudnative/health-connect");
+const healthCheck = new health.HealthChecker();
+
+const livePromise = () =>
+  new Promise((resolve, _reject) => {
+    const appFunctioning = true;
+    if (appFunctioning) {
+      resolve();
+    } else {
+      reject(new Error("App is not functioning correctly"));
+    }
+  });
+
+const liveCheck = new health.LivenessCheck("LivenessCheck", livePromise);
+healthCheck.registerLivenessCheck(liveCheck);
+
+const readyCheck = new health.PingCheck("example.com");
+healthCheck.registerReadinessCheck(readyCheck);
+
+app.use("/live", health.LivenessEndpoint(healthCheck));
+app.use("/ready", health.ReadinessEndpoint(healthCheck));
+app.use("/health", health.HealthEndpoint(healthCheck));
+
 app.get("/", (req, res) => {
   res.send({
     timestamp: new Date(),
